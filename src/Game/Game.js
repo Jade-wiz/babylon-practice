372843dddeb1820fd714c0d06cf792c8b875904a
Jade Kim <jade@Jadeui-iMac.local>
2020-11-13 17:36:23 +0900

    Initial commit

---
+++ src/Game/Game.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { Engine, Scene, Skybox } from 'react-babylonjs';
+import { Vector3 } from '@babylonjs/core';
+
+let globalIndex = 0 // due to closure and how observables are assigned.
+const SkyboxScenes = [{
+  name: 'sunny day',
+  texture: `${process.env.PUBLIC_URL}/textures/TropicalSunnyDay`
+}, {
+  name: 'specular HDR',
+  texture: `${process.env.PUBLIC_URL}/textures/SpecularHDR.dds`
+}]
+
+function Game() {
+    globalIndex = 0
+
+    return (
+        <div style={{width : '100vw'}}>
+            <Engine antialias={true} adaptToDeviceRatio={true} canvasId="sample-canvas">
+            <Scene>
+              <hemisphericLight name="hemi-light" intensity={0.7} direction={Vector3.Up()} />
+              <Skybox rootUrl={SkyboxScenes[0].texture} />
+              <arcRotateCamera target={ Vector3.Zero() } radius={10}
+                alpha={-Math.PI / 2} beta={(Math.PI / 2)} minZ={0.001} wheelPrecision={50}
+              />
+            </Scene>
+          </Engine>
+        </div>
+    )
+}
+
+export default Game
